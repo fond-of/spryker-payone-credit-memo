bc3e6f08ed15bab11313784e24ab67dca66c5861
Julian Zimmermann <julian.zimmermann@fondof.de>
2020-08-21 12:21:45 +0200

    - added logging - refactoring

--- src/FondOfSpryker/Zed/PayoneCreditMemo/Communication/Plugin/CreditMemoExtension/Processor/PayoneCreditMemoProcessorPlugin.php
+++ src/FondOfSpryker/Zed/PayoneCreditMemo/Communication/Plugin/CreditMemoExtension/Processor/PayoneCreditMemoProcessorPlugin.php
@@ -7,6 +7,8 @@ use FondOfSpryker\Zed\CreditMemoExtension\Dependency\Plugin\CreditMemoProcessorP
 use FondOfSpryker\Zed\PayoneCreditMemo\Exception\NoRefundableItemsFoundException;
 use Generated\Shared\Transfer\CreditMemoProcessorStatusTransfer;
 use Generated\Shared\Transfer\CreditMemoTransfer;
+use Propel\Runtime\Collection\ObjectCollection;
+use Spryker\Shared\Log\LoggerTrait;
 use Spryker\Zed\Kernel\Communication\AbstractPlugin;
 
 /**
@@ -16,10 +18,10 @@ use Spryker\Zed\Kernel\Communication\AbstractPlugin;
  */
 class PayoneCreditMemoProcessorPlugin extends AbstractPlugin implements CreditMemoProcessorPluginInterface
 {
+    use LoggerTrait;
+
     public const NAME = 'PayoneCreditMemoProcessorPlugin';
-
     public const LISTENING_PAYMENT_PROVIDER = 'Payone';
-
     public const LISTENING_PAYMENT_METHOD = [
         'payment.payone.creditcard',
         'payment.payone.e_wallet',
@@ -29,33 +31,30 @@ class PayoneCreditMemoProcessorPlugin extends AbstractPlugin implements CreditMe
      * @param \Generated\Shared\Transfer\CreditMemoTransfer $creditMemoTransfer
      * @param \Generated\Shared\Transfer\CreditMemoProcessorStatusTransfer $statusResponse
      *
-     * @throws \FondOfSpryker\Zed\PayoneCreditMemo\Exception\NoRefundableItemsFoundException
-     *
      * @return \Generated\Shared\Transfer\CreditMemoProcessorStatusTransfer|null
      */
     public function process(
         CreditMemoTransfer $creditMemoTransfer,
         CreditMemoProcessorStatusTransfer $statusResponse
     ): ?CreditMemoProcessorStatusTransfer {
-        $statusResponse->setWasRefunded(false);
         if ($this->canProcess($creditMemoTransfer) === true) {
             try {
-                $items = $this->getFactory()->getCreditMemoFacade()->getSalesOrderItemsByCreditMemo($creditMemoTransfer);
-                if ($items === null) {
-                    throw new NoRefundableItemsFoundException(sprintf(
-                        'No refundable items found for CreditMemo with id %s and order reference %s',
-                        $creditMemoTransfer->getIdCreditMemo(),
-                        $creditMemoTransfer->getOrderReference()
-                    ));
-                }
-                $this->getFactory()->getOmsFacade()->triggerEvent(
+                $items = $this->resolveItemsToRefund($creditMemoTransfer);
+                $status = $this->getFactory()->getOmsFacade()->triggerEvent(
                     CreditMemoProcessorPluginInterface::EVENT_NAME,
                     $items,
                     []
                 );
+                $statusResponse->setMessage('internal oms failure');
+
+                if ($status !== null) {
+                    $statusResponse->setMessage('');
+                    $statusResponse->setSuccess(true);
+                }
             } catch (Exception $exception) {
                 $statusResponse->setMessage($exception->getMessage());
                 $statusResponse->setSuccess(false);
+                $this->getLogger()->error($exception->getMessage(), $exception->getTrace());
             }
         }
 
@@ -82,5 +81,27 @@ class PayoneCreditMemoProcessorPlugin extends AbstractPlugin implements CreditMe
         return $salesPaymentMethodType !== null
             && $salesPaymentMethodType->getPaymentProvider() === static::LISTENING_PAYMENT_PROVIDER
             && in_array($salesPaymentMethodType->getPaymentMethod(), static::LISTENING_PAYMENT_METHOD);
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\CreditMemoTransfer $creditMemoTransfer
+     *
+     * @throws \FondOfSpryker\Zed\PayoneCreditMemo\Exception\NoRefundableItemsFoundException
+     *
+     * @return \Propel\Runtime\Collection\ObjectCollection
+     */
+    protected function resolveItemsToRefund(CreditMemoTransfer $creditMemoTransfer): ObjectCollection
+    {
+        $items = $this->getFactory()->getCreditMemoFacade()->getSalesOrderItemsByCreditMemo($creditMemoTransfer);
+
+        if ($items === null) {
+            throw new NoRefundableItemsFoundException(sprintf(
+                'No refundable items found for CreditMemo with id %s and order reference %s',
+                $creditMemoTransfer->getIdCreditMemo(),
+                $creditMemoTransfer->getOrderReference()
+            ));
+        }
+
+        return $items;
     }
 }
